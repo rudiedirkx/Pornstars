df84e717614be1ae6197255e4dbe7d5fa9af4796
Rudie Dirkx <github@hotblocks.nl>
2017-04-10 00:22:08 +0200

    cfgs+

--- public_html/_project/cfg_r_d_results.php
+++ public_html/_project/cfg_r_d_results.php
@@ -3,69 +3,79 @@
 require_once '../inc.bootstrap.php';
 
 $types = ['travel_eta', 'r_d_eta', 'r_d_costs', 'fuel_use', 'offense', 'defence'];
-$types = array_merge(array_values($db->select_fields('d_resources', "id, CONCAT('income:', id)", '1')), $types);
+$types = $db->select_fields('d_resources', "CONCAT('income:', id), CONCAT(resource, ' income')", '1') + array_combine($types, $types);
 
+$units = ['real', 'pct'];
+$units = array_combine($units, $units);
+
+// Save
 if ( isset($_POST['r']) ) {
 	foreach ( $_POST['r'] AS $id => $data ) {
 		$data['enabled'] = !empty($data['enabled']);
-		$db->update('d_r_d_results', $data, compact('id'));
-	}
-
-	if ( isset($_POST['new']) ) {
-		$new = array_filter($_POST['new']);
-		if ( isset($new['type'], $new['done_r_d_id'], $new['change'], $new['unit'], $new['explanation']) ) {
-			$new['enabled'] = true;
-			$db->insert('d_r_d_results', $new);
+		if ( $id ) {
+			$db->update('d_r_d_results', $data, compact('id'));
+		}
+		elseif ( $data['type'] && $data['done_r_d_id'] && $data['explanation'] ) {
+			$data['o'] or $data['o'] = $db->max('d_r_d_results', 'o') + 1;
+			$db->insert('d_r_d_results', $data);
 		}
 	}
 
 	return do_redirect(null);
 }
 
-echo '<title>R&D results</title>';
+$RD = $db->select_fields('d_r_d_available', "id, concat(UPPER(T), ' ', id, '. ', name)", '1 ORDER BY T, id');
 
-$arrRDResults = $db->select('d_r_d_results', '1 ORDER BY o ASC, id DESC');
-$RD = $db->select_fields('d_r_d_available', 'id, concat(UPPER(T),\' \',id,\'. \',name)', '1 ORDER BY T ASC, id ASC');
-
-echo '<form method="post" action="" autocomplete="off"><table border="1" cellpadding="4" cellspacing="1">';
-foreach ( $arrRDResults AS $k => $r ) {
-	echo '<tr><th>[' . $r['id'] . '] Type</th><td><select name="r[' . $r['id'] . '][type]">';
-	foreach ( $types AS $t ) {
-		$selected = $t == $r['type'] ? 'selected' : '';
-		echo '<option value="' . $t . '" ' . $selected . '>' . $t . '</option>';
-	}
-	echo '</select></td></tr>';
-	echo '<tr><th>R&D</th><td><select name="r[' . $r['id'] . '][done_r_d_id]"><option value="">--</option>';
-	foreach ( $RD AS $iRD => $szRD ) {
-		$selected = $iRD == $r['done_r_d_id'] ? 'selected' : '';
-		echo '<option value="' . $iRD . '" ' . $selected . '>' . $szRD . '</option>';
-	}
-	echo '</select></td></tr>';
-	echo '<tr><th>Change</th><td><input type="text" size="8" name="r[' . $r['id'] . '][`change`]" value="' . $r['change'] . '" /></td></tr>';
-	echo '<tr><th>Unit</th><td><select name="r[' . $r['id'] . '][unit]"><option value="">--</option><option'.( 'real' == $r['unit'] ? ' selected="1"' : '' ).' value="real">real</option><option'.( 'pct' == $r['unit'] ? ' selected="1"' : '' ).' value="pct">pct</option></select></td></tr>';
-	echo '<tr><th>Explanation</th><td><input type="text" size="40" name="r[' . $r['id'] . '][explanation]" value="' . $r['explanation'] . '" /></td></tr>';
-	echo '<tr><th>`Order`</th><td><input type="text" size="8" name="r[' . $r['id'] . '][o]" value="' . $r['o'] . '" /></td></tr>';
-	echo '<tr><th>Enabled</th><td><input type="checkbox" name="r[' . $r['id'] . '][enabled]" value="1"'.( '1' === $r['enabled'] ? ' checked="1"' : '' ).' /></td></tr>';
-	echo '<tr><th colspan="2" bgcolor="#666666"></td></tr>';
-}
-
-echo '<tr><th colspan="2" bgcolor="#999999">NEW:</td></tr>';
-echo '<tr><th>Type</th><td><select name="new[type]"><option value="">--</option>';
-foreach ( $types AS $t ) {
-	echo '<option value="' . $t.'">' . $t.'</option>';
-}
-echo '</select></td></tr>';
-echo '<tr><th>R&D</th><td><select name="new[done_r_d_id]"><option value="">--</option>';
-foreach ( $RD AS $iRD => $szRD ) {
-	echo '<option value="' . $iRD.'">' . $szRD.'</option>';
-}
-echo '</select></td></tr>';
-echo '<tr><th>Change</th><td><input type="text" size="8" name="new[change]" value="0" /></td></tr>';
-echo '<tr><th>Unit</th><td><select name="new[unit]"><option value="">--</option><option value="real">real</option><option value="pct">pct</option></select></td></tr>';
-echo '<tr><th>Explanation</th><td><input type="text" size="40" name="new[explanation]" value="" /></td></tr>';
-echo '<tr><th>`Order`</th><td><input type="text" size="8" name="new[o]" value="0" /></td></tr>';
-echo '<tr><th colspan="2"><input type="submit" value="Opslaan" /></td></tr>';
-echo '</table></form>';
+$arrRDResults = $db->select('d_r_d_results', '1 ORDER BY o ASC, id DESC')->all();
+$arrRDResults[] = new db_generic_record(['id' => '0']);
 
 ?>
-<script type="text/javascript">document.forms[0].reset();</script>
+<title>R&D results</title>
+
+<form method="post" action autocomplete="off">
+	<table border="1" cellpadding="4" cellspacing="1">
+		<?php
+		foreach ( $arrRDResults AS $n => $r ) {
+			$dummy = $r->id ? null : '--';
+
+			if ( $n ) {
+				echo '<tr>';
+				echo '<th colspan="2" bgcolor="#666666"></td>';
+				echo '</tr>';
+			}
+
+			echo '<tr>';
+			echo '<th>[' . $r['id'] . '] Type</th>';
+			echo '<td><select name="r[' . $r['id'] . '][type]">' . html_options($types, $r['type'], $dummy) . '</select></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>R&D</th>';
+			echo '<td><select name="r[' . $r['id'] . '][done_r_d_id]">' . html_options($RD, $r['done_r_d_id'], $dummy) . '</select></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>Change</th>';
+			echo '<td><input type="text" size="8" name="r[' . $r['id'] . '][`change`]" value="' . $r['change'] . '" /></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>Unit</th>';
+			echo '<td><select name="r[' . $r['id'] . '][unit]">' . html_options($units, $r['unit'], $dummy) . '</select></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>Explanation</th>';
+			echo '<td><input type="text" size="40" name="r[' . $r['id'] . '][explanation]" value="' . $r['explanation'] . '" /></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>`Order`</th>';
+			echo '<td><input type="text" size="8" name="r[' . $r['id'] . '][o]" value="' . $r['o'] . '" /></td>';
+			echo '</tr>';
+			echo '<tr>';
+			echo '<th>Enabled</th>';
+			echo '<td><input type="checkbox" name="r[' . $r['id'] . '][enabled]" value="1"'.( '1' === $r['enabled'] ? ' checked="1"' : '' ).' /></td>';
+			echo '</tr>';
+		}
+		?>
+		<tr>
+			<th colspan="2"><input type="submit" value="Opslaan" /></td>
+		</tr>
+	</table>
+</form>
